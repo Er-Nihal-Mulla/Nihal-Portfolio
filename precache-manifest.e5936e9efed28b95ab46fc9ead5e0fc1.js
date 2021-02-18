cd1fc341fba96256524afba923da39a2edf00e2e
Neel9784 <neel9784@gmail.com>
2021-02-18 17:51:43 +0530

    Updates

---
+++ precache-manifest.e5936e9efed28b95ab46fc9ead5e0fc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f728942337d1f3042e51f4b3a4aa2d07",
+    "url": "/Nihal-Portfolio/index.html"
+  },
+  {
+    "revision": "61be1ae8e38f127b3949",
+    "url": "/Nihal-Portfolio/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "64d776ee0316dc7923de",
+    "url": "/Nihal-Portfolio/static/css/main.300acb78.chunk.css"
+  },
+  {
+    "revision": "61be1ae8e38f127b3949",
+    "url": "/Nihal-Portfolio/static/js/2.d0c54171.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Nihal-Portfolio/static/js/2.d0c54171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d776ee0316dc7923de",
+    "url": "/Nihal-Portfolio/static/js/main.26a6e9ae.chunk.js"
+  },
+  {
+    "revision": "1a7fd65b8d2505a7534f",
+    "url": "/Nihal-Portfolio/static/js/runtime-main.529dbd3c.js"
+  }
+]);
